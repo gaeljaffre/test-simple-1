c614da156c6c2c8d6583a0e0aadb475fbe0d9661
gaeljaffre <gael.jaffre@yahoo.fr>
2020-01-29 13:57:55 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
-import { ObjetService } from './objet.service';
+import { ObjetService, Cat, url } from './objet.service';
+
+// à enlever
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
@@ -10,9 +14,17 @@ import { ObjetService } from './objet.service';
 
 export class AppComponent  {
   name = 'Angular';
+  url = url;
+  urltab = "https://Simple1.voyagesgael.repl.co/tab";
+  posts: any[];
+
+  objet;
+  cat: Cat;
 
   constructor(
-    private objetService: ObjetService
+    private objetService: ObjetService,
+    // XXX à enlever
+    private http: HttpClient
   ) {
 
   }
@@ -20,11 +32,84 @@ export class AppComponent  {
   ngOnInit() {
   }
 
+  getTestDirect() {
+    let nom;
+
+    this.http.get(url + "tab").subscribe(
+      (data: any[]) => {
+          console.log('data: ' + data);
+          if(data.length){
+            console.log('data: ' + data);
+            nom = data[0].name;
+          }
+      }
+    );
+    console.log("nom = " + nom);
+    return nom;
+  }
+
+  getTestDirect2() {
+    //var v = this.http.get(this.urltab).pipe(map((res:Response) => res.json()));
+    console.log("lecture de " + this.urltab);
+    var v = this.http.get(this.urltab).subscribe((data: any) =>       
+     {
+        console.log(data);
+        this.cat = {
+          id: data['id'],
+          name: data['name'],
+          numero: data['numero']
+        }
+     });;
+    console.log(this.cat);
+    //console.log(this.cat.name);
+  }
+
+  getTest() {
+      console.log("ici");
+      this.objetService.getTexte().subscribe((data)=>{
+        console.log(data);
+        this.objet = data['objet'];
+        //window.alert('');
+    });
+  }
+
   getTexte() {
-    return this.objetService.getTexte();
+    let cat: Cat;
+    this.objetService.getTexte().subscribe((data: Cat) => cat = {
+        name: data['name']
+      }
+    );
+
+    return cat.name;
+  }
+
+  post() {
+    this.objetService.postTexte();
+  }
+
+  getReponse() {
+    let variable: any;
+    this.objetService.getResponse().subscribe((data) => variable = {
+        response: data['response']
+      }
+    );
+
+  console.log(variable);
+    return variable;
+  }
+
+
+  clear() {
+    document.getElementById("zone1").innerText = "";
   }
 
   setTexte(texte) {
-    document.getElementById("zone1").innerHTML = texte;
+    document.getElementById("zone1").innerText = texte;
   }
+
+  setLectureRest() {
+    this.setTexte(this.getTexte());
+  }
+
+
 }
--- src/app/objet.service.ts
+++ src/app/objet.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 
-const url = 'https://us-central1-telephone1-41de6.cloudfunctions.net/app/';
+//export const url = 'https://us-central1-telephone1-41de6.cloudfunctions.net/app/';
+
+export const url = 'https://Simple1.voyagesgael.repl.co/';
 
 export interface Cat {
-  name: string
+  id: number,
+  name: string,
+  numero: number
 }
 
 @Injectable()
@@ -15,9 +19,29 @@ export class ObjetService {
       private http: HttpClient
     ) {}
 
-  getTexte(): Observable<string> {
-    return this.http.get<string>(url);
+  getTexte(): Observable<Cat> {
+    //let chat = this.http.get<Cat>(url + 'tab');
+    let chat = this.http.get(url + 'tab');
+    return chat;
   }
+
+  getResponse() {
+    let reponse = this.http.get(url + 'tab');
+    return reponse;
+  }
+
+  postTexte() {
+    let v = this.http.post(url + 'post', {name: 'Minou-Chat'});
+    console.log(v);
+    return v;
+  }
+
+  postTexte2() {
+    let v = this.http.post(url, {name: 'Minou-Chat'});
+    console.log(v);
+    return v;
+  }
+
 
 /*
   getAllCats(): Observable<Cat[]> {
